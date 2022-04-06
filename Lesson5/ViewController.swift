6b06d14ceb4a6b836ead7030a6f7cdb842defec2
Tigran Karakhanyan <lev.98@bk.ru>
2022-04-06 15:55:09 +0400

    Initial Commit

---
+++ Lesson5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson5
+//
+//  Created by Karakhanyan Tigran on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
